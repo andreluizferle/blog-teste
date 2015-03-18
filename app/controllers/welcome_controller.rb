a5f4cb73849d24d32f75e0966fe18b67b0289030
Ferle <andreluizferle@AndreMac.home>
2015-03-18 12:22:30 -0400

    Add Donation

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,28 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def donate
+    Iugu.api_key = "98f7ca6cc1b969430492d0c8378fc4ce"
+
+    charge = Iugu::Charge.create({
+      "token" => params[:token],
+      "email" => "andre@iugu.com",
+      "items" => [
+        {
+          "description" => "Donate Blog",
+          "quantity" => "1",
+          "price_cents" => "500"
+        }
+      ]
+    })
+
+    if charge.success
+      redirect_to thank_you_url
+    else
+      flash[:danger] = "Erro processando pagamento!"
+      redirect_to root_path
+    end
+  end
+
 end
